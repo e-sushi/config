06fd661e59cb954ff3d32d4c3e1b7660fa46a588
sushi <officialaxos7@gmail.com>
2023-09-26 06:47:07 +0000

    setup keys for searching lsp symbols and saving

--- nvim/keys.lua
+++ nvim/keys.lua
@@ -5,5 +5,9 @@ vim.keymap.set('n', '<c-h>', '<cmd>HopAnywhere<cr>');
 
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>');
 
+vim.keymap.set('n', '<a-w>', '<cmd>w<cr>');
+
+vim.keymap.set('n', '<c-s>', '<cmd>Telescope lsp_document_symbols<cr>');
+vim.keymap.set('n', '<a-s>', '<cmd>Telescope lsp_workspace_symbols<cr>');
 
 vim.keymap.set('n', '<F12>', function() vim.lsp.buf.definition({reuse_win=true}) end);
